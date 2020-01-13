31b42a270d8319ff230be7cb297b7591d77d859c
Tal Heber <dsf@sdf.com>
2020-01-12 21:25:28 -0800

    finish part 1

--- ex11.py
+++ ex11.py
@@ -24,29 +24,87 @@ class Diagnoser:
 		self.root = root
 		
 	def diagnose(self, symptoms):
-		self.diagnose_helper(root, symptoms)
+		return self.diagnose_helper(root, symptoms)
 
 	def diagnose_helper(self,node, symptoms):
 		if (node.positive_child == None) and node.negative_child == None:
 			return node.data
 
 		if node.data in symptoms:
-			node.data = node.positive_child
 			return self.diagnose_helper(node.positive_child, symptoms)
 		else:
-			node.data = node.negative_child
 			return self.diagnose_helper(node.negative_child, symptoms)
-		
 
 	def calculate_success_rate(self, records):
-		pass
-		
+	#Record (illness, symptoms)
+		counter = 0
+		for record in records:
+			if self.diagnose(record[1]) == record[0]:
+				counter += 1
+		success_rate = counter/len(records)
+		return success_rate
+
 	def all_illnesses(self):
-		pass
-		
+		list_all_disease = self.all_illnesses_helper(root)
+		d = dict()
+		for illnesse in list_all_disease:
+			if illnesse not in d:
+				d[illnesse] = 1
+			else:
+				d[illnesse] += 1
+
+		items = list(d.items())
+
+		items.sort(key =lambda item:item[1], reverse = True)
+
+		return items
+
+
+	def all_illnesses_helper(self, node):
+		if (node.positive_child == None) and node.negative_child == None:
+			return [node.data]
+
+		x = self.all_illnesses_helper(node.positive_child)
+		y = self.all_illnesses_helper(node.negative_child)
+		return x+y
+
 	def paths_to_illness(self, illness):
-		pass
-		
+		return self.paths_to_illness_helper(illness,self.root)
+
+
+	def paths_to_illness_helper(self, illness,node):
+		if ((node.positive_child == None) and node.negative_child == None):
+			if illness == node.data:
+				return []
+			else:
+				return None
+
+		x = self.paths_to_illness_helper(illness,node.positive_child)
+		y = self.paths_to_illness_helper(illness,node.negative_child)
+
+		if x != None:
+			if x == []:
+				x = [[True]]
+			else:
+				for i in x:
+					i.insert(0,True)
+		if y != None:
+			if y == []:
+				y = [[False]]
+			else:
+				for i in y:
+					i.insert(0,False)
+
+		if x == None and y == None:
+			return []
+		if y == None and x != None:
+			return x
+		if x == None and y != None:
+			return y
+		else:
+			return x+y
+
+
 
 def build_tree(records, symptoms):
 	pass
@@ -71,7 +129,9 @@ if __name__ == "__main__":
 	root = Node("cough", inner_vertex, healthy_leaf)
 	
 	diagnoser = Diagnoser(root)
-	
+	print(diagnoser.paths_to_illness("cold"))
+
+
 	# Simple test
 	diagnosis = diagnoser.diagnose(["cough"])
 	if diagnosis == "cold":
